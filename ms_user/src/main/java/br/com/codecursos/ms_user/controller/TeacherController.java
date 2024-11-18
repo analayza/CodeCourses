9de4547fd466581169b4b08f9c81faa3fee7d1e6
HeyNicollasHey <joao.nicollas.dev@gmail.com>
2024-11-18 10:56:27 -0300

    controller e service de student e teacher

--- ms_user/src/main/java/br/com/codecursos/ms_user/controller/TeacherController.java
+++ ms_user/src/main/java/br/com/codecursos/ms_user/controller/TeacherController.java
@@ -1,4 +1,31 @@
 package br.com.codecursos.ms_user.controller;
 
+import br.com.codecursos.ms_user.domain.Student;
+import br.com.codecursos.ms_user.domain.Teacher;
+import br.com.codecursos.ms_user.dto.TeacherDTO;
+import br.com.codecursos.ms_user.service.TeacherService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/teacher")
+@RequiredArgsConstructor
 public class TeacherController {
+
+    private final TeacherService teacherService;
+
+    @PostMapping("/save")
+    public ResponseEntity<Teacher> save(@RequestBody TeacherDTO teacherDTO){
+        return ResponseEntity.status(HttpStatus.CREATED).body(teacherService.save(teacherDTO));
+    }
+
+    @PostMapping("/delete")
+    public ResponseEntity<Teacher> deleteById(Long id){
+        return ResponseEntity.status(HttpStatus.OK).body((teacherService.deleteById(id)));
+    }
 }
