34196be57e6c6ca636710ce75930096792f261b7
analayza <analayza00@gmail.com>
2024-11-21 16:36:20 -0300

    Atualizando

--- ms_courses/src/main/java/br/com/codecursos/ms_courses/mapper/ModuleClassMapper.java
+++ ms_courses/src/main/java/br/com/codecursos/ms_courses/mapper/ModuleClassMapper.java
@@ -4,7 +4,6 @@ import br.com.codecursos.ms_courses.domain.ModuleClass;
 import br.com.codecursos.ms_courses.dto.ModuleClassDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-
 @Mapper(componentModel = "spring")
 public interface ModuleClassMapper {
     @Mapping(target = "id", ignore = true)
--- ms_courses/src/main/java/br/com/codecursos/ms_courses/repository/CourseModuleRepository.java
+++ ms_courses/src/main/java/br/com/codecursos/ms_courses/repository/CourseModuleRepository.java
@@ -1,10 +1,12 @@
 package br.com.codecursos.ms_courses.repository;
 import br.com.codecursos.ms_courses.domain.CourseModule;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 public interface CourseModuleRepository extends JpaRepository<CourseModule, Long> {
+
     @Modifying
     @Query("UPDATE CourseModule c SET " +
             "c.title = COALESCE(:title, c.title), " +
