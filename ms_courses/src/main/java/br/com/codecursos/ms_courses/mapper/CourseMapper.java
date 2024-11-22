e266edcd291398d093b8634cfe2d7475ea34e8f8
HeyNicollasHey <joao.nicollas.dev@gmail.com>
2024-11-21 22:09:14 -0300

    teste

--- ms_courses/src/main/java/br/com/codecursos/ms_courses/mapper/CourseMapper.java
+++ ms_courses/src/main/java/br/com/codecursos/ms_courses/mapper/CourseMapper.java
@@ -5,7 +5,6 @@ import br.com.codecursos.ms_courses.dto.CourseDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-
 @Mapper(componentModel = "spring")
 public interface CourseMapper {
 
