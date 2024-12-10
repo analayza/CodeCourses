ec8397cdc469a6c089a42c1b1808e4f6254ed7ce
HeyNicollasHey <joao.nicollas.dev@gmail.com>
2024-12-10 13:32:14 -0300

    notacao do login

--- ms_user/src/main/java/br/com/codecursos/ms_user/controller/LoginController.java
+++ ms_user/src/main/java/br/com/codecursos/ms_user/controller/LoginController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/login")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class LoginController {
 
     private final StudentService studentService;
